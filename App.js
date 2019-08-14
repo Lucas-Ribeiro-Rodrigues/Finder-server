1ac3425952004c9a0bb35ef3821792d2ccb963aa
LUCAS RIBEIRO RODRIGUES <u16186@academico.cotuca.unicamp.br>
2019-08-14 13:43:02 -0300

    Firebase configured successfully

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
 const express = require('express');
-
+var admin = require("firebase-admin");
+var serviceAccount = require("./config/firebaseKey.json");
 const app = express();
 
+
+
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://finder-88f51.firebaseio.com"
+});
+
+var db = admin.firestore();
+var ref = db.collection("users").doc('alantu');
+ref.set({
+  alanisawesome: {
+    date_of_birth: "June 23, 1912",
+    full_name: "Alan Turing"
+  }
+});
 app.listen(3000, () => {
-    console.log("server listening on port 3000");
+    console.log(ref.get());
 });
 
 app.get('/', (req, res) => {
