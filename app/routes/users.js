faa102caf578049d856937cdde43165821cdc700
Lucas Ribeiro Rodrigues <lucasr6@yahoo.com.br>
2019-08-15 12:39:47 -0300

    Add post for user signup

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,12 +1,12 @@
 const db = require("../../config/firebaseConfig");
 
 module.exports = app => {
-    const collection = db.collection("users");
+    const usersCollection = db.collection("users");
 
     app.post('/users-management/user-login', async (req, res) => {
         if(req.body != undefined)
         {
-            const documents = await collection.get();
+            const documents = await usersCollection.get();
             let user = null;
             for(let i=0; i < documents.docs.length; i++)
             {
@@ -35,6 +35,25 @@ module.exports = app => {
         {
             res.status(406).send("request body not found");
         }    
+    })
+
+    app.post("/users-management/user-signup", async (req,res) => {
+        if(req.body)
+        {
+            let user = {
+                "Nick": req.body.Nick,
+                "Password": req.body.Password, 
+            }
+            const firebaseReturn = await usersCollection.add(user);
+            if(firebaseReturn)
+                res.send(`Vaga ${firebaseReturn.id} adicionada com sucesso`);
+            else
+                res.status(500).send("Erro ao cadastrar usuario");
+        }
+        else
+        {
+            res.status(406).send("request body not found");
+        }
     })
 
     const extractUser = (doc) => {
