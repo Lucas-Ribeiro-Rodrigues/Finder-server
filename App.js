d28f31ef110147b7e783c4c26c373eb677e67229
Lucas Ribeiro Rodrigues <lucasr6@yahoo.com.br>
2019-10-12 11:21:12 -0300

    Trying hard to make heroku work

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 const express   = require('express');
 const server    = require("./config/server");
 const port      = process.env.PORT | 3000;
-const host      = process.env.MY_HOST | '0.0.0.0';
+const host      = '0.0.0.0';
 
 server.listen(port, host, () => {
     console.log(`Server listening on port ${port}, at host ${host}`);
